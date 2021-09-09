557905d83081c6e2f9b05694909ed24ab19e12ce
Hiroshi Yagui <hiroshiyagui@gmail.com>
2021-09-09 10:17:52 -0500

    make it better

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
-<?php
-header("Location:login.php");
-exit();
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pagina</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   </head>
+<body>
+  <nav> 
+    <div class="menu">
+      <div class="logo">
+        <a href="#">Blues Bus</a>
+      </div>
+      <ul>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="Empresas.html">Empresas</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="login.html">Iniciar Sesion</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="img"></div>
+  <div class="center">
+    <div class="title">BLUES BUS </div>
+    <div class="sub_title">Siempre Confiable</div>
+    <div class="btns">
+      <a class="button" href="login.html">Informacion</a>
+      <a class="button" href="register.html">Registrarse</a>
+    </div>
+  </div>
+</body>
 
-?>
+</html>
--- login.php
+++ login.php
@@ -1,119 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include 'dbcon.php';
 
-<head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="au theme template">
-    <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
-
-    <!-- Title Page-->
-    <title>Login</title>
-
-    <!-- Fontfaces CSS-->
-    <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-
-    <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/theme.css" rel="stylesheet" media="all">
-
-</head>
-
-<body class="animsition">
-    <div class="page-wrapper">
-        <div class="page-content--bge5">
-            <div class="container">
-                <div class="login-wrap">
-                    <div class="login-content">
-                        <div class="login-logo">
-                            <a href="#">
-                                <img src="images/icon/logo.png" alt="CoolAdmin">
-                            </a>
-                        </div>
-                        <?php
-                        if(!empty($_REQUEST['error'])){
-                            if($_REQUEST['error']=='1' ){
-                            echo '<div class="alert alert-danger" role="alert">Datos erróneos, vuelva a ingresarlos </div>';
-                            }else if($_REQUEST['error']=='1' ){
-                                echo '<div class="alert alert-danger" role="alert">Problemas internos del servidor, espere un rato </div>';
-                            }
-                        }
-                        ?>
-                        <div class="login-form">
-                            <form action="login_cliente.php" method="post">
-                                <div class="form-group">
-                                    <label>Correo Electronico</label>
-                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
-                                </div>
-                                <div class="form-group">
-                                    <label>Contraseña</label>
-                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Contraseña">
-                                </div>
-                                <div class="login-checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember">Recordarme
-                                    </label>
-                                    <label>
-                                        <a href="#">¿Olvidaste tu Contraseña?</a>
-                                    </label>
-                                </div>
-                                <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">Registrarse</button>
-                            </form>
-                            <div class="register-link">
-                                <p>
-                                    ¿No tienes cuenta? 
-                                    <a href="register.php">Registrate aquí</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-    <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
-    </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-    </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
-    </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js">
-    </script>
-
-    <!-- Main JS-->
-    <script src="js/main.js"></script>
-
-</body>
-
-</html>
-<!-- end document-->
+    $v1=$_REQUEST['uname'];
+    $v2=$_REQUEST['psw'];  
+    
+    $sql ="SELECT id,contra FROM `usuarios` WHERE usuario='".$v1."'";
+    $resultado = mysqli_query($conn, $sql);
+    if(!$resultado){
+        echo "Incorrecto";
+    }else{
+        
+        $fetchpsw=mysqli_fetch_array($resultado);
+        $decpsw=md5($v2);
+        if($decpsw==$fetchpsw['contra']){
+            session_start();
+            $_SESSION['user']=$v1;
+            if($fetchpsw['id']==19){
+                header('Location:PanelAdmin.php');
+                exit;
+            }
+            header('Location:PanelUsuario.php');
+            exit;
+        }else{       
+            header("Location:index.php?msg=1");
+            exit;
+        }
+    }
+?>
